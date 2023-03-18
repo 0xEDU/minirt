dc31bc4574b1bc1cc705a7920f8b5e208a920f0a
etachott <etachott@student.42sp.or.br>
2023-03-18 19:33:30 -0300

    docs: added possible action plan to follow

--- sources/main.c
+++ sources/main.c
@@ -6,12 +6,21 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 13:50:59 by edu               #+#    #+#             */
-/*   Updated: 2023/03/11 21:14:31 by edu              ###   ########.fr       */
+/*   Updated: 2023/03/18 19:32:31 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+/*
+*	1. Validate input.
+*	2. Load .rt file in a t_raytracer struct (?)
+*	3. Generate a scene using the struct...?
+*	...
+*/
+
+// TODO:
+// - Encapsulate t_mlx mlx in t_raytracer.
 int	main(void)
 {
 	t_mlx	mlx;
