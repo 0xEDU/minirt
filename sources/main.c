17c673330043ccf6b2c7881a5672b56898ebff82
Guilherme Ribeiro <guilherme_f_ribeiro@hotmail.com>
2023-03-20 19:48:25 -0300

    feat(img): added colored images
    Co-authored-by: 0xEDU <bfwaend@gmail.com>

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: guribeir <guribeir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 13:50:59 by edu               #+#    #+#             */
-/*   Updated: 2023/03/20 19:30:08 by guribeir         ###   ########.fr       */
+/*   Updated: 2023/03/20 19:47:26 by guribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,23 @@
 int	main(void)
 {
 	t_minirt	minirt;
+	int			i;
+	int			j;
 
 	mlx_open_window(&minirt.mlx);
-
+	mlx_create_image(&minirt);
+	i = 0;
+	while (i < WIDTH)
+	{
+		j = 0;
+		while (j < 	HEIGHT)
+		{
+			mlx_pixel_draw(&minirt.img, i, j, 0x0000FF);
+			j++;
+		}
+		i++;
+	}
+	mlx_image_to_window(&minirt, 0, 0);
 	mlx_loop_window(&minirt.mlx);
 	return (0);
 }
