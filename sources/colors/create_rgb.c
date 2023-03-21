62985985df2fe2151e0778c0d8519de4730d2f11
Guilherme Ribeiro <guilherme_f_ribeiro@hotmail.com>
2023-03-21 18:03:26 -0300

    refactor: moved r g b to vector struct

--- sources/colors/create_rgb.c
+++ sources/colors/create_rgb.c
@@ -6,13 +6,20 @@
 /*   By: guribeir <guribeir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 16:53:40 by guribeir          #+#    #+#             */
-/*   Updated: 2023/03/21 16:54:08 by guribeir         ###   ########.fr       */
+/*   Updated: 2023/03/21 17:15:17 by guribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-unsigned long	create_rgb(int r, int g, int b)
+unsigned long	create_rgb(t_color *color)
 {
+	int	r;
+	int	g;
+	int	b;
+
+	r = color->r;
+	g = color->g;
+	b = color->b;
 	return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
 }
