ab3cfad1b5301c2748c483283274765cff9555ce
etachott <bfwaend@gmail.com>
2023-04-24 12:30:45 -0300

    norm: render_scene on norm, kinda

--- sources/render_scene.c
+++ sources/render_scene.c
@@ -6,7 +6,7 @@
 /*   By: etachott < etachott@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 20:07:55 by guribeir          #+#    #+#             */
-/*   Updated: 2023/04/19 13:40:47 by etachott         ###   ########.fr       */
+/*   Updated: 2023/04/24 12:26:42 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,12 @@ static t_ray	ray_constructor(t_camera *camera, t_vector normal)
 	t_ray		new;
 
 	new.origin = camera->lookfrom;
-	// normal.y = 1.0 - normal.y;
 	new.direction = normalize(vector_diff(
-			vector_sum(
-				vector_sum(camera->lower_left_corner,
-					vector_mult(camera->horizontal, normal.x)),
-				vector_mult(camera->vertical, normal.y)),
-			camera->lookfrom));
+				vector_sum(
+					vector_sum(camera->lower_left_corner,
+						vector_mult(camera->horizontal, normal.x)),
+					vector_mult(camera->vertical, normal.y)),
+				camera->lookfrom));
 	return (new);
 }
 
@@ -48,12 +47,12 @@ static void	turn_on_camera(t_camera *camera, double aspect_ratio)
 	t_vector	u;
 	t_vector	v;
 
-	if (vector_is_equal(camera->lookat, vector_create(0,1,0)))
-	    camera->lookat.z += 0.0001;
-	else if (vector_is_equal(camera->lookat, vector_create(0,-1,0)))
-	    camera->lookat.z -= 0.0001;	
+	if (vector_is_equal(camera->lookat, vector_create(0, 1, 0)))
+		camera->lookat.z += 0.0001;
+	else if (vector_is_equal(camera->lookat, vector_create(0, -1, 0)))
+		camera->lookat.z -= 0.0001;
 	theta = convert_to_radians(camera->fov);
-	h_cam = tan(theta/2);
+	h_cam = tan(theta / 2);
 	camera->view_up = vector_create(0, 1, 0);
 	camera->viewport_height = 1.175 * h_cam;
 	camera->viewport_width = aspect_ratio * camera->viewport_height;
@@ -64,7 +63,8 @@ static void	turn_on_camera(t_camera *camera, double aspect_ratio)
 	camera->vertical = vector_mult(v, camera->viewport_height);
 	camera->lower_left_corner = vector_diff(
 			vector_diff(
-				vector_diff(camera->lookfrom, vector_div(vector_negate_self(&camera->horizontal), 2)),
+				vector_diff(camera->lookfrom,
+					vector_div(vector_negate_self(&camera->horizontal), 2)),
 				vector_div(camera->vertical, 2)), w);
 }
 
