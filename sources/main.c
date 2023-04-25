ec214a0817d1300d6094f3c79a2bc8f5ffea6f83
etachott <bfwaend@gmail.com>
2023-04-25 16:46:07 -0300

    feat: moved world pointer to minirt

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: etachott < etachott@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 13:50:59 by edu               #+#    #+#             */
-/*   Updated: 2023/04/19 21:13:40 by etachott         ###   ########.fr       */
+/*   Updated: 2023/04/25 14:16:20 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,11 @@ int	main(int argc, char **argv)
 
 	if (!validate_argv(argc, argv) || !validate_file(argv[1]))
 		return (-1);
-	printf("Passou da validaçao\n");
 	init_minirt(&minirt);
 	mlx_open_window(&minirt.mlx);
 	mlx_create_image(&minirt);
 	world = create_world(argv[1], &minirt);
-	// printf("AMBIENT = %f | %f %f %f", minirt.ambient.ratio, minirt.ambient.color.r, minirt.ambient.color.g, minirt.ambient.color.b);
+	minirt.world = world;
 	render_scene(&minirt, world);
 	mlx_image_to_window(&minirt, 0, 0);
 	printf("DONE :D\n");
