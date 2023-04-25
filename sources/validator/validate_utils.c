ec39a588caa862b66f1f94693fa11e6b8304036d
etachott <bfwaend@gmail.com>
2023-04-25 12:42:13 -0300

    fix: normal validation now less buggy

--- sources/validator/validate_utils.c
+++ sources/validator/validate_utils.c
@@ -6,38 +6,54 @@
 /*   By: etachott < etachott@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 19:17:07 by etachott          #+#    #+#             */
-/*   Updated: 2023/04/24 21:21:34 by etachott         ###   ########.fr       */
+/*   Updated: 2023/04/25 12:40:05 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+static	int	comma_check(char *line)
+{
+	int	i;
+	int	comma;
+
+	i = 0;
+	comma = 0;
+	while (line[i])
+	{
+		if (line[i] == ',')
+			comma++;
+		i++;
+	}
+	if (comma != 2)
+		return (0);
+	return (1);
+}
+
+
 int	validate_normal(char *normal)
 {
 	// Refatorar
 	int		i;
-	int		comma;
+	char	**split;
 
 	i = -1;
-	comma = 0;
-	while (normal[++i])
-		if (normal[i] == ',')
-			comma++;
-	if (comma != 2)
+	if (!comma_check(normal))
 		return (0);
-	if (ft_strcmp(normal, "1,0,0") == 0)
-		return (1);
-	else if (ft_strcmp(normal, "-1,0,0") == 0)
-		return (1);
-	else if (ft_strcmp(normal, "0,1,0") == 0)
-		return (1);
-	else if (ft_strcmp(normal, "0,-1,0") == 0)
-		return (1);
-	else if (ft_strcmp(normal, "0,0,-1") == 0)
-		return (1);
-	else if (ft_strcmp(normal, "0,0,1") == 0)
-		return (1);
-	return (0);
+	while (normal[++i])
+	{
+		if (!ft_isdigit(normal[i]) && normal[i] != '.'
+				&& normal[i] != ',' && normal[i] != '-')
+			return (0);
+	}
+	split = ft_split(normal, ',');
+	i = -1;
+	while (split[++i])
+	{
+		if (ft_atof(split[i]) != 0 && ft_atof(split[i]) != 1)
+			return (clean_return(0, (void **)split));
+	}
+	return (clean_return(1, (void **)split));
 }
 
 int	validate_double(char *line)
@@ -91,24 +107,6 @@ int	validate_vector(char **vector)
 	while (vector[++i])
 		if (!validate_double(vector[i]))
 			return (0);
-	return (1);
-}
-
-static	int	comma_check(char *line)
-{
-	int	i;
-	int	comma;
-
-	i = 0;
-	comma = 0;
-	while (line[i])
-	{
-		if (line[i] == ',')
-			comma++;
-		i++;
-	}
-	if (comma != 2)
-		return (0);
 	return (1);
 }
 
