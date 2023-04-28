aacc767875f5536700f9b373f19e3930f587c76c
Guilherme Ribeiro <guilherme_f_ribeiro@hotmail.com>
2023-04-27 21:08:19 -0300

    fix(cylinder): caps are now working fine
    Co-authored-by: 0xEDU <bfwaend@gmail.com>

--- sources/hit_cylinder.c
+++ sources/hit_cylinder.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   hit_cylinder.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: etachott < etachott@student.42sp.org.br    +#+  +:+       +#+        */
+/*   By: guribeir <guribeir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 14:26:45 by etachott          #+#    #+#             */
-/*   Updated: 2023/04/27 18:58:50 by etachott         ###   ########.fr       */
+/*   Updated: 2023/04/27 21:05:24 by guribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,6 @@ static int	hit_caps_helper(t_hit *hit, double t_val,
 		hit->rec->has_normal = 1;
 		if (is_bottom)
 			hit->rec->normal = vector_mult(hit->cyl->axis, -1);
-			// hit->rec->normal = hit->cyl->axis;
 		else
 			hit->rec->normal = hit->cyl->axis;
 		set_face_normal(hit->rec, hit->ray, &hit->rec->normal);
@@ -41,58 +40,95 @@ static int	hit_caps_helper(t_hit *hit, double t_val,
 	return (0);
 }
 
-static int	hit_caps(t_cylinder cyl, t_ray *ray,
-	t_variation t, t_hit_record *rec)
+static int hit_caps(t_cylinder cyl, t_ray *ray, t_variation t, t_hit_record *rec)
 {
-	double		t_bottom;
-	double		t_top;
-	t_hit		hit;
-	t_vector	point_bottom;
-	t_vector	point_top;
+	double t_bottom;
+	double t_top;
+	t_hit hit;
+	t_vector point_bottom;
+	t_vector point_top;
+	int hit_bottom;
+	int hit_top;
+	t_vector axis_origin_to_bottom;
+	t_vector axis_origin_to_top;
+	double denom_bottom;
+	double denom_top;
 
 	hit.cyl = &cyl;
 	hit.ray = ray;
 	hit.t = t;
 	hit.rec = rec;
-	t_bottom = (vector_dot(cyl.cap_bottom, cyl.axis) - vector_dot(ray->origin,
-				cyl.axis)) / vector_dot(ray->direction, cyl.axis);
-	t_top = (vector_dot(cyl.cap_top, cyl.axis) - vector_dot(ray->origin,
-				cyl.axis)) / vector_dot(ray->direction, cyl.axis);
+	axis_origin_to_bottom = vector_diff(cyl.cap_bottom, ray->origin);
+	axis_origin_to_top = vector_diff(cyl.cap_top, ray->origin);
+	denom_bottom = vector_dot(ray->direction, cyl.axis);
+	denom_top = vector_dot(ray->direction, cyl.axis);
+	if (fabs(denom_bottom) > 1e-6)
+		t_bottom = vector_dot(axis_origin_to_bottom, cyl.axis) / denom_bottom;
+	else
+		t_bottom = -1;
+	if (fabs(denom_top) > 1e-6)
+		t_top = vector_dot(axis_origin_to_top, cyl.axis) / denom_top;
+	else
+		t_top = -1;
 	point_bottom = ray_at(*ray, t_bottom);
 	point_top = ray_at(*ray, t_top);
-	if (hit_caps_helper(&hit, t_bottom, point_bottom, 1))
-		return (1);
-	if (hit_caps_helper(&hit, t_top, point_top, 0))
-		return (1);
+	hit_bottom = hit_caps_helper(&hit, t_bottom, point_bottom, 1);
+	hit_top = hit_caps_helper(&hit, t_top, point_top, 0);
+	if (hit_bottom && hit_top)
+	{
+		if (t_bottom < t_top)
+			return hit_caps_helper(&hit, t_bottom, point_bottom, 1);
+		else
+			return hit_caps_helper(&hit, t_top, point_top, 0);
+	}
+	else if (hit_bottom)
+		return hit_caps_helper(&hit, t_bottom, point_bottom, 1);
+	else if (hit_top)
+		return hit_caps_helper(&hit, t_top, point_top, 0);
 	return (0);
 }
 
-static int	select_hit(double rt[2], t_hit *hit)
+static int select_hit(double rt[2], t_hit *hit)
 {
-	t_vector	p[2];
-	double		h[2];
+    t_vector p[2];
+    double h[2];
+    int valid_intersection[2] = {0, 0};
 
-	p[0] = ray_at(*hit->ray, rt[0]);
-	h[0] = vector_dot(hit->cyl->axis, vector_diff(p[0], hit->cyl->cap_bottom));
-	p[1] = ray_at(*hit->ray, rt[1]);
-	h[1] = vector_dot(hit->cyl->axis, vector_diff(p[1], hit->cyl->cap_bottom));
-	if ((h[0] >= 0 && h[0] <= hit->cyl->height && rt[0] >= hit->t.min
-			&& rt[0] <= hit->t.max) && (h[1] < 0 || h[1] > hit->cyl->height
-			|| rt[1] < hit->t.min || rt[1] > hit->t.max || rt[0] < rt[1]))
-	{
-		hit->rec->t = rt[0];
-		hit->rec->point = p[0];
-		return (1);
-	}
-	else if ((h[1] >= 0 && h[1] <= hit->cyl->height && rt[1] >= hit->t.min
-			&& rt[1] <= hit->t.max) && (h[0] < 0 || h[0] > hit->cyl->height
-			|| rt[0] < hit->t.min || rt[0] > hit->t.max || rt[1] < rt[0]))
-	{
-		hit->rec->t = rt[1];
-		hit->rec->point = p[1];
-		return (1);
-	}
-	return (0);
+    p[0] = ray_at(*hit->ray, rt[0]);
+    h[0] = vector_dot(hit->cyl->axis, vector_diff(p[0], hit->cyl->cap_bottom));
+    p[1] = ray_at(*hit->ray, rt[1]);
+    h[1] = vector_dot(hit->cyl->axis, vector_diff(p[1], hit->cyl->cap_bottom));
+
+    valid_intersection[0] = (h[0] >= 0 && h[0] <= hit->cyl->height && rt[0] >= hit->t.min && rt[0] <= hit->t.max);
+    valid_intersection[1] = (h[1] >= 0 && h[1] <= hit->cyl->height && rt[1] >= hit->t.min && rt[1] <= hit->t.max);
+
+    if (valid_intersection[0] && valid_intersection[1])
+    {
+        if (rt[0] < rt[1])
+        {
+            hit->rec->t = rt[0];
+            hit->rec->point = p[0];
+        }
+        else
+        {
+            hit->rec->t = rt[1];
+            hit->rec->point = p[1];
+        }
+        return (1);
+    }
+    else if (valid_intersection[0])
+    {
+        hit->rec->t = rt[0];
+        hit->rec->point = p[0];
+        return (1);
+    }
+    else if (valid_intersection[1])
+    {
+        hit->rec->t = rt[1];
+        hit->rec->point = p[1];
+        return (1);
+    }
+    return (0);
 }
 
 static int	body_or_cap(t_hit *hit, int body_hit)
